6ec2278cfedb1b925f8d56d05cfd585c7a7a879d
nathaank <nathankayk45@gmail.com>
2022-05-05 20:41:50 -0300

    feat(client): create client class

---
+++ src/core/infra/Client.ts
@@ -0,0 +1,14 @@
+import { Client as DiscordClient, ClientOptions } from 'discord.js';
+
+type CustomClientOptions = ClientOptions & {
+  token: string;
+}
+
+export class Client extends DiscordClient {
+
+  constructor (options: CustomClientOptions) {
+    super(options);
+
+    this.token = options.token;
+  }
+}
